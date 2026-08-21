567b5fe24968620d2093342adb213f106b8b1d9f
EddieKe <otienoaudrey1@gmail.com>
2026-08-21 15:18:20 +0300

    Restructed the services

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/@tabler/icons-webfont/3.1.0/tabler-icons.min.css"
+          href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css"
         />
       </head>
       <body className="min-h-full flex flex-col font-preahvihear">
